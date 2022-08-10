31e839d824cb3558d94f3f06badafb1aec0544ea
Namu Sazerat <namulabre@gmail.com>
2022-08-11 01:28:02 +0200

    Refactoring des fonctions de suppression de projet

--- src/supprimer_projet.rs
+++ src/supprimer_projet.rs
@@ -4,9 +4,11 @@ pub fn supprimer_projet(nom : String){
 
 fn remplacer_fichier_information(nouveau_contenu_fichier : String){
     println!("Mise à jour du fichier d'information du Framework...");
-
+    //1. On supprime le fichier d'information, sachant qu'on a déja récupérer son contenu
     std::fs::remove_file("projets\\projets.info").expect("Erreur durant la suppression du fichier d'information.");
+    //2. On le recréer
     std::fs::File::create("projets\\projets.info").expect("Erreur durant la régénération du fichier d'information.");
+    //3. On replace le contenu de l'ancien fichier (sans le projet qui a été supprimé) dans le nouveau fichier créer, comme si de rien n'était.
     std::fs::write("projets\\projets.info", nouveau_contenu_fichier).expect("Erreur durant le re-remplissage du fichier d'information.");
 
     println!("fini.");
@@ -19,14 +21,11 @@ fn supprimer_fichier_projet(chemin_projet : String){
 fn enlever_la_ligne_du_projet_dans_fichier_information(nom : String){
     let contenu_fichier = std::fs::read_to_string("projets\\projets.info").expect("Erreur durant la lecture du fichier listant les projets du framework.");
     let mut contenu_fichier_sans_projet = String::new();
-    println!(".info : {}", contenu_fichier.clone());
-    if contenu_fichier.contains(&nom) {
-        println!("le projet existe !");
+    if le_nom_du_projet_est_dans_le_fichier(nom.clone(), contenu_fichier.clone()) {
+        println!("Le nom du projet est valide.");
         for ligne in std::fs::read_to_string("projets\\projets.info").unwrap().lines(){
-            println!("ligne:{}", ligne.clone());
             if !ligne.contains(&nom) {
                 contenu_fichier_sans_projet = contenu_fichier_sans_projet + ligne;
-                println!("cfsp :{}", contenu_fichier_sans_projet.clone());
             }
             else{
                 let mut iteration = 0;
@@ -45,4 +44,8 @@ fn enlever_la_ligne_du_projet_dans_fichier_information(nom : String){
     else{
         panic!("Le projet est introuvable");
     }
+}
+//On vérifie que le nom du projet et dans le contenu du fichier.
+fn le_nom_du_projet_est_dans_le_fichier(nom_projet : String, contenu_fichier : String) -> bool{
+    return contenu_fichier.contains(&nom_projet);
 }
