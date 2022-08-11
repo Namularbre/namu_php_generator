0ec44b5d6ee62d01ba3e426db8e01924dcf1819c
Namu Sazerat <namulabre@gmail.com>
2022-08-11 13:54:12 +0200

    Pour ajouter une connexion à une base de données, on utilise le nom du projet, et pas son chemin

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ struct Options{
 
 fn main() {
     let options = Options::from_args();
-
     if options.action == "nouveau" || options.action == "n" {
         let chemin_projet = options.parametres[0].clone();
         let nom_projet = options.parametres[1].clone();
@@ -21,12 +20,14 @@ fn main() {
         arborescence.creer_arborescence().expect("Il y a eu une erreur durant la création du nouveau projet.");
         println!("Votre projet est prêt ! Il vous attends :3");
     }
+    //Pourquoi ça marche pas ?
     else if options.action == "connexion_bdd" || options.action == "bdd"{
         let hote = options.parametres[0].clone();
         let nom_bdd = options.parametres[1].clone();
         let utilisateur = options.parametres[2].clone();
         let mdp_uti = options.parametres[3].clone();
-        let racine = std::fs::read_to_string("C:/Users/namul/testage/namu_php_framework.info").unwrap();
+        let nom_projet = options.parametres[4].clone();
+        let racine = trouver_chemin_projet(nom_projet);
         creer_connexion_bdd::creer_connexion_bdd(racine ,hote, nom_bdd, utilisateur, mdp_uti);
     }
     else if options.action == "supprimer" || options.action == "s"{
@@ -36,4 +37,20 @@ fn main() {
     else{
         println!("L'action choisi n'est pas reconnue :/");
     }
+}
+
+fn trouver_chemin_projet(nom_projet : String) -> String{
+    let lignes_fichier_info = std::fs::read_to_string("projets\\projets.info").unwrap();
+    for ligne in lignes_fichier_info.lines(){
+        if ligne.contains(&nom_projet){
+            let mut iteration = 0;
+            for champs in ligne.split_whitespace(){
+                if iteration == 1{
+                    return champs.to_owned();
+                }
+                iteration += 1;
+            }
+        }
+    }
+    panic!("Impossible de trouver le chemin du projet.");
 }
