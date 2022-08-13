8dfe342f65c4d94c286a0222f5becd6ed4573266
Namu Sazerat <namulabre@gmail.com>
2022-08-13 16:05:09 +0200

    Refactoring du code de création de connexion à une BDD

--- src/creer_connexion_bdd.rs
+++ src/creer_connexion_bdd.rs
@@ -3,20 +3,43 @@ use std::fs::File;
 use std::fs::write;
 
 pub fn creer_connexion_bdd(chemin_projet : String, hote : String, nom_bdd : String, utilisateur : String, code : String){
-    println!("Création du dossier pour la connexion...");
+    if creer_fichier_connexion_bdd(chemin_projet.clone()).is_ok(){
+        let contenu_fichier_bdd = preparer_contenu_classe_bdd(hote, nom_bdd, utilisateur, code);
+        let chemin_fichier_classe_bdd = avoir_chemin_fichier_classe_bdd(chemin_projet);
+        if remplir_fichier_classe_bdd(contenu_fichier_bdd, chemin_fichier_classe_bdd).is_ok(){
+            println!("Opération terminée !");
+        }
+        else{
+            panic!("Il y a eu une erreur durant le ramplissage du fichier de la classe bdd (Bdd.php)");
+        }
+    }
+}
+
+fn creer_fichier_connexion_bdd(chemin_projet : String) -> std::io::Result<()>{
+    println!("Création des fichiers et dossier nécessaire...");
     let chemin_dossier_connexion_bdd = chemin_projet.clone() + "/connexion_bdd";
     create_dir_all(chemin_dossier_connexion_bdd.clone()).expect("Erreur durant la création du dossier qui contiendra le système de connexion à la base de données.");
-    println!("fin");
-
+    
     let chemin_fichier_classe_bdd = chemin_dossier_connexion_bdd.clone() + "/Bdd.php";
-    println!("Création du fichier qui contiendra la classe de la BDD...");
     File::create(chemin_fichier_classe_bdd.clone()).expect("Erreur durant la création du fichier contenant la classe Bdd");
-    let mut contenu_sample_fichier_bdd = std::fs::read_to_string("samples\\bdd.php.sample").unwrap();
+    return Ok(());
+}
+
+fn avoir_chemin_fichier_classe_bdd(chemin_projet : String) -> String{
+    return (chemin_projet.clone() + "/connexion_bdd/Bdd.php").clone();
+}
+
+fn preparer_contenu_classe_bdd(hote : String, nom_bdd : String, utilisateur : String, code : String) -> String{
+    let mut contenu_sample_fichier_bdd = std::fs::read_to_string("samples/bdd.php.sample").unwrap();
     contenu_sample_fichier_bdd = str::replace(&contenu_sample_fichier_bdd, "HOTE", &hote);
     contenu_sample_fichier_bdd = str::replace(&contenu_sample_fichier_bdd, "NOM_BDD", &nom_bdd);
     contenu_sample_fichier_bdd = str::replace(&contenu_sample_fichier_bdd, "UTILISATEUR", &utilisateur);
     contenu_sample_fichier_bdd = str::replace(&contenu_sample_fichier_bdd, "MDP", &code);
-    
-    write(chemin_fichier_classe_bdd, contenu_sample_fichier_bdd).expect("Erreur durant le remplissage du fichier de la classe BDD.");
-    println!("Opération terminée !");
+
+    return contenu_sample_fichier_bdd;
+}
+
+fn remplir_fichier_classe_bdd(contenu_fichier_bdd : String, chemin_fichier_classe_bdd : String) -> std::io::Result<()>{
+    write(chemin_fichier_classe_bdd, contenu_fichier_bdd).expect("Erreur durant le remplissage du fichier de la classe BDD.");
+    return Ok(());
 }
